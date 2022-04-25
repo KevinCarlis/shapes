1bfc7373831db7e79e687190440726b25993d83d
Kevin Carlis <kevcarlis@gmail.com>
2022-04-24 20:34:56 -0700

    tests

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for x in {0..9};
+do
+    if [ `./a.out < ./tests/inputfile$x | diff -B -w - ./tests/outputfile$x | wc -l` == 0 ]; then
+        echo "Test $x passed";
+    else
+        echo "Test $x failed";
+        ./a.out < ./tests/inputfile$x | diff -B -w - ./tests/outputfile$x
+    fi
+done
